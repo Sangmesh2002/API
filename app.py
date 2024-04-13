7dc2e04e3f61a81966b0c6e5bb0bc812cb2a03a2
sangmesh Deshmukh <89828829+Mr-coder09@users.noreply.github.com>
2024-04-13 11:18:39 +0530

    lfs

---
+++ app.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from sklearn.ensemble import RandomForestRegressor
+import joblib
+
+# Define FastAPI app
+app = FastAPI()
+
+# Load the trained model
+model = joblib.load("model.pkl")
+
+# Define input data schema using Pydantic BaseModel
+class InputData(BaseModel):
+    state: int
+    district: int
+    commodity: int
+    month: int
+    season: int
+
+
+@app.post("/predict")
+async def predict_price(data: InputData):
+  
+    input_data = [[data.state, data.district, data.commodity, data.month,data.season]]
+    
+    prediction = model.predict(input_data)
+    # Return prediction as response
+    return {"predicted_price": prediction[0]}
+
+# Run the FastAPI server
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
